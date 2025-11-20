d25e1d7535a6df3820fe11f594f263d9d57e1204
cardoza2 <adam@cardoza.one>
2025-11-20 16:01:14 -0700

    remove manifests and extra packages.

--- src/WATT.jl
+++ src/WATT.jl
@@ -1,11 +1,10 @@
 module WATT
 
-using FLOWMath, LinearAlgebra, StaticArrays, CurveFit, NLsolve, UnPack
+using FLOWMath, LinearAlgebra, StaticArrays, CurveFit, NLsolve, UnPack #Todo: Am I really using CurveFit? 
 using CCBlade, GXBeam, DynamicStallModels
 using ImplicitAD, ForwardDiff, ReverseDiff
-using Infiltrator #Todo: Remove
 using DelimitedFiles #Todo: Why is this here? 
-using Plots #Todo: Convert to RecipesBase
+using Plots 
 #Todo: Why do I have plots as a dependency? -> So I can plot mid simulation. I wonder if there is a way to pass plots in or something. 
 #Todo: Why do I have NLsolve as a dependency? -> I think for the BDF solvers... but I don't know if I need that. 
 #Todo: Add the solution parameters as arguments to the user functions. 
